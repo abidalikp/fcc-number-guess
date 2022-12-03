e660e18a3ee2c3a52fbe42b8f935c5e94d6aa46e
abidalikp <abidalikp98@gmail.com>
2022-12-03 06:32:31 +0000

    feat: welcome message

--- number_guess.sh
+++ number_guess.sh
@@ -5,3 +5,17 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 # Generate random secret number
 SECRET_NUMBER=$((1 + $RANDOM % 10))
 echo $SECRET
+
+echo Enter your username:
+read USERNAME
+
+# Get best_game
+BEST_GAME=$($PSQL "select best_game from users where username='$USERNAME';")
+
+# Check its existance
+if [[ -z $BEST_GAME ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
