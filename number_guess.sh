e2200bec0e055abed8ca7f147be9f624b921a51a
abidalikp <abidalikp98@gmail.com>
2022-12-03 07:17:21 +0000

    fix: GAME

--- number_guess.sh
+++ number_guess.sh
@@ -19,9 +19,10 @@ MAIN () {
   then
     echo "Welcome, $USERNAME! It looks like this is your first time here."
   else
-    echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+    echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
   fi
 
+  NUMBER_OF_GUESSES = 0
   GAME "Guess the secret number between 1 and 1000:"
 
 }
@@ -30,23 +31,36 @@ GAME () {
 
   echo $1
   read GUESS_NUMBER
+  # echo $GUESS_NUMBER
 
-  if [[ $GUESS_NUMBER =~ "^[0-9]+$" ]]
+  # Check guessed number is number
+  if [[ ! $GUESS_NUMBER =~ ^[0-9]+$ ]]
   then
+   
+    GAME "That is not an integer, guess again:"
+
+  else
+
+    # Increment #guess
+    ((NUMBER_OF_GUESSES++))
 
     if [[ $GUESS_NUMBER > $SECRET_NUMBER ]]
     then
+
       GAME "It's lower than that, guess again:"
+    
     elif [[ $GUESS_NUMBER < $SECRET_NUMBER ]]
     then
-      GAME "It's higher than that, guess again:"
-    else
-      echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
-    fi
 
-  else
-  
-    GAME "That is not an integer, guess again:"
+      GAME "It's higher than that, guess again:"
+    
+    else
+      
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+      # insert into users
+    
+    fi
 
   fi
 
